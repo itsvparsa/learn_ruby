c932465dfda956fe0fceb3d5c66a96fc33001211
itsvparsa <itsvparsa@gmail.com>
2017-12-27 00:28:01 +0000

    convert to array

--- arrays/convert_range_array.rb
+++ arrays/convert_range_array.rb
@@ -4,5 +4,10 @@ p letters_array.length # 26
 p letters_array[0..2] # ["A", "B", "C"]
 p letters_array.include?("V") # t
 
-
+numbers = 301..355
+numbers_array = numbers.to_a
+p numbers_array.class # Array
+p numbers_array.is_a?(Array) # t
+p numbers_array.size # 55
+p numbers_array[0] # 301
 
